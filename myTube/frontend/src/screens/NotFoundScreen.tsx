cf36e24f5a7a0a2749d6f9ebf5bce8fd930dbe4c
adriroca20 <hardhe21@gmail.com>
2024-04-18 01:55:41 +0200

    Finished notFoundScreen component

--- myTube/frontend/src/screens/NotFoundScreen.tsx
+++ myTube/frontend/src/screens/NotFoundScreen.tsx
@@ -1,7 +1,8 @@
 export const NotFoundScreen = () => {
     return (
-        <div>
-            <h1>404 - Not Found</h1>
+        <div className="flex h-screen justify-center items-center text-slate-200 flex-col gap-5">
+            <h1 className="text-3xl font-bold">Work in progress, come back later!</h1>
+            <img src="/workInProgress.webp" alt="404" />
         </div>
     )
 }
